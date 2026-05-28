50dfb7f38828f94fa7fd01815b88071e8a6d8a3d
lujubru <carlos.lucas.reta@gmail.com>
2026-05-27 22:03:02 -0300

    Update application name to 'Black Guardian'

--- frontend/src/components/Layout.js
+++ frontend/src/components/Layout.js
@@ -74,7 +74,7 @@ const Layout = () => {
             <div className="flex items-center space-x-8">
               <div className="flex items-center space-x-2">
                 <img src="/logo.png" alt="aplicacion" className="h-10 w-10 rounded-lg object-contain" />
-                <span className={`font-bold text-lg ${darkMode ? 'text-white' : 'text-gray-900'}`}>aplicacion</span>
+                <span className={`font-bold text-lg ${darkMode ? 'text-white' : 'text-gray-900'}`}>Black Guardian</span>
               </div>
               <div className="hidden md:flex space-x-1">
                 {navItems.map((item) => {
